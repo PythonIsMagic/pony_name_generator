a890d560104c9509ba9fc68414a43038635700a4
Erik Lunna <eslunna@gmail.com>
2017-05-02 19:12:02 -0500

    Created a skeleton for the main file.

---
+++ namegen.py
@@ -0,0 +1,26 @@
+""" namegen.py
+    Generates random and interesting pony names.
+"""
+
+
+def get_name(word_dict):
+    """ Creates a random name from a dictionary. """
+
+
+def import_words():
+    """ Collects all the words from the data files.
+        Creates a dictionary of nouns, verbs, and adjectives.
+    """
+    # Get nouns
+    # Get adjectives
+    # Get verbs
+    pass
+
+
+def main():
+    """ Main run loop """
+    print("Welcome to Erik's Pony Name Generator!")
+
+
+if __name__ == "__main__":
+    main()
