513c095b01175df4ea97e08e1e0e86df466c6704
Erik Lunna <eslunna@gmail.com>
2017-05-09 22:47:39 -0500

    Started a test file for namegen.

---
+++ test_namegen.py
@@ -0,0 +1,22 @@
+import namegen
+
+
+# Tests for def pluralize_noun(n):
+def test_pluralize_noun_bat():
+    result = namegen.pluralize_noun('bat')
+    assert result == 'bats'
+
+
+def test_pluralize_noun_sky():
+    result = namegen.pluralize_noun('sky')
+    assert result == 'skies'
+
+
+# Tests for def rhyme(inp, level):
+# Tests for def is_ryhme(word1, word2):
+# Tests for def find_rhyme(word):
+
+# Meh - don't care so much about testing these...
+# def get_name(word_dict):
+# def scan_files(prefix):
+# def import_words():
